4c7986688f0ac73bcc3429032d1951d16e19be6f
yangweijie <917647288@qq.com>
2020-04-04 16:28:59 +0800

    调整index 应用

--- app/index/home/Index.php
+++ app/index/home/Index.php
@@ -3,10 +3,14 @@ declare (strict_types = 1);
 
 namespace app\index\home;
 
-class Index
+class Index extends Home
 {
     public function index()
     {
-        return '您好！这是一个[index]示例应用';
+        // 默认跳转模块
+        if (config('app.home_default_module') != '' && config('app.home_default_module') != 'index') {
+            $this->redirect(config('app.home_default_module'). '/index/index');
+        }
+        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> '.config("dolphin.product_name").' '.config("dolphin.product_version").'<br/><span style="font-size:30px">极速 · 极简 · 极致</span></p></div>';
     }
 }
