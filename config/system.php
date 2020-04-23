afd20407c67df2b5963f409cb13a1cce38fa30ff
yangweijie <917647288@qq.com>
2020-04-23 16:24:57 +0800

    模块管理排除install

--- config/system.php
+++ config/system.php
@@ -11,5 +11,5 @@ return [
     // 拒绝ie访问
     'deny_ie'       => false,
     // 模块管理中，不读取模块信息的目录
-    'except_module' => ['common', 'admin', 'index', 'extra', 'user']
+    'except_module' => ['common', 'admin', 'index', 'extra', 'user', 'install']
 ];
